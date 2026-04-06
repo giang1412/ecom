0472c9b39474e94167033381ebdc63fec0f1c67b
Ho Ngoc Giang <giaq1412@gmail.com>
2026-04-06 16:41:20 +0700

    feat(auth): implement PaymentAPIKeyGuard and update authentication flow

--- src/routes/payment/payment.controller.ts
+++ src/routes/payment/payment.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common'
 import { PaymentService } from './payment.service'
 import { MessageResDTO } from 'src/shared/dtos/response.dto'
 import { ZodSerializerDto } from 'nestjs-zod'
-import { IsPublic } from 'src/shared/decorators/auth.decorator'
+import { Auth, IsPublic } from 'src/shared/decorators/auth.decorator'
 import { WebhookPaymentBodyDTO } from 'src/routes/payment/payment.dto'
 
 @Controller('payment')
@@ -11,7 +11,7 @@ export class PaymentController {
 
   @Post('/receiver')
   @ZodSerializerDto(MessageResDTO)
-  @IsPublic()
+  @Auth(['PaymentAPIKey'])
   receiver(@Body() body: WebhookPaymentBodyDTO) {
     return this.paymentService.receiver(body)
   }
