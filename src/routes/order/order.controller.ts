bef47e60d5347b670728e2684cccba11fc81b456
Ho Ngoc Giang <giaq1412@gmail.com>
2026-04-06 13:56:23 +0700

    feat(order): implement create, detail, and cancel order functionalities in order service and repository

--- src/routes/order/order.controller.ts
+++ src/routes/order/order.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common'
 import { ZodSerializerDto } from 'nestjs-zod'
-import { GetOrderListQueryDTO, GetOrderListResDTO } from 'src/routes/order/order.dto'
+import {
+  CancelOrderResDTO,
+  CreateOrderBodyDTO,
+  CreateOrderResDTO,
+  GetOrderDetailResDTO,
+  GetOrderListQueryDTO,
+  GetOrderListResDTO,
+  GetOrderParamsDTO,
+} from 'src/routes/order/order.dto'
 import { OrderService } from 'src/routes/order/order.service'
 import { ActiveUser } from 'src/shared/decorators/active-user.decorator'
+import { EmptyBodyDTO } from 'src/shared/dtos/request.dto'
 
 @Controller('orders')
 export class OrderController {
@@ -10,7 +19,25 @@ export class OrderController {
 
   @Get()
   @ZodSerializerDto(GetOrderListResDTO)
-  getCart(@ActiveUser('userId') userId: number, @Query() query: GetOrderListQueryDTO) {
+  getOrders(@ActiveUser('userId') userId: number, @Query() query: GetOrderListQueryDTO) {
     return this.orderService.list(userId, query)
+  }
+
+  @Post()
+  @ZodSerializerDto(CreateOrderResDTO)
+  create(@ActiveUser('userId') userId: number, @Body() body: CreateOrderBodyDTO) {
+    return this.orderService.create(userId, body)
+  }
+
+  @Get(':orderId')
+  @ZodSerializerDto(GetOrderDetailResDTO)
+  detail(@ActiveUser('userId') userId: number, @Param() param: GetOrderParamsDTO) {
+    return this.orderService.detail(userId, param.orderId)
+  }
+
+  @Put(':orderId')
+  @ZodSerializerDto(CancelOrderResDTO)
+  cancel(@ActiveUser('userId') userId: number, @Param() param: GetOrderParamsDTO, @Body() _: EmptyBodyDTO) {
+    return this.orderService.cancel(userId, param.orderId)
   }
 }
